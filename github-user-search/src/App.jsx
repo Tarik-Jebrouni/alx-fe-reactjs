78b9a3ab07661eceefed1664ff01fe9242704e03
Tarik-Jebrouni <tarikjebrouni7@gmail.com>
2025-08-02 16:55:44 +0100

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GitHub User Search</h1>
+      <p>Start searching for GitHub users...</p>
+    </div>
+  );
+}
+
+export default App;
