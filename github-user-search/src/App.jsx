6525c68e9da4f7443f347a36f458464b09feaee1
Tarik-Jebrouni <70601551+Tarik-Jebrouni@users.noreply.github.com>
2025-08-03 20:39:54 +0100

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,12 @@
 import "./index.css";
 import React from "react";
-import SearchBar from "./components/Search";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="app">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1>GitHub User Search</h1>
-      <SearchBar />
+      <Search />
     </div>
   );
 }
