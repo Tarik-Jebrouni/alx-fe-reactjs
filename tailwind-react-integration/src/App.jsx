6666c2161151e39afb438c2621978df29e168c26
Tarik-Jebrouni <tarikjebrouni7@gmail.com>
2025-08-04 19:24:28 +0100

    Add styled UserProfile component using Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-blue-600 text-center mt-10">
-      ✅ Tailwind fonctionne !
+    <div>
+      <UserProfile />
     </div>
   );
 }
