c9bd8d8732ec5261094353ea9af5251128616fa7
Tarik-Jebrouni <tarikjebrouni7@gmail.com>
2025-08-04 17:58:44 +0100

    Setting Up Tailwind CSS in a New React Project

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,7 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
